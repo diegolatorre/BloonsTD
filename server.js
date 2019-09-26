4408f71beccbecbf67980f2e94bd29b7c22f1ade
diegolatorre <df.lima98@gmail.com>
2019-09-26 16:55:04 -0300

    criacao do server node

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+http.listen(port, () => {
+    console.log(`starting server at: ${port}`);
+});
